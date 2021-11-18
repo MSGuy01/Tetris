c569ce0deb6ddef6d66831eee095ee76f80724cd
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2021-11-18 08:31:06 -0800

    stuff

--- js/block.js
+++ js/block.js
@@ -1,4 +1,23 @@
-function Block(type, gridData, src) {
-	//TYPES: 1: I-BLOCK | 2: J-BLOCK | 3: L-BLOCK | 4: O-BLOCK | 5: S-BLOCK | 6: T-BLOCK | 7: Z-BLOCK
-	
+var c = document.getElementById("game");
+var ctx = c.getContext("2d");
+var bl = 30;
+class Block {
+	constructor() {
+		this.y = 0;
+		this.fill = function() {
+			ctx.fillStyle = 'red';
+			ctx.fillRect(0, y, bl, bl);
+		}
+		this.fall = function() {
+			window.setInterval(() => {
+				y += 30;
+				this.fill();
+			}, 100);
+		}
+	}
 }
+$('#start').on('click', () => {
+	const b = new Block();
+	b.fill();
+	b.fall();
+});
